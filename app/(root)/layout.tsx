273e20fad03b40915749f12f9cf8da6ba7499a6c
Wilson Chan <wilsonchan54@gmail.com>
2025-08-23 22:54:04 -0400

    Created layout.tsx within root route group / created Navbar and Sidebar components (empty placeholders)

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,37 @@
+
+import Sidebar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
+
+interface DashboardLayout{
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayout) => {
+  return (
+    <div className="min-h-screen">
+      
+      <div className="flex w-full h-full">
+
+        {/* Left Side */}
+        <div className="bg-green-800 fixed left-0 top-0 hidden lg:block lg-w-[270px] h-full overflow-y-auto">
+          <Sidebar />
+        </div>
+
+        {/* Right Side */}
+        <div className="bg-blue-800 lg:pl-[270px] w-full">
+          <div className="mx-auto max-w-screen-2xl h-full">
+
+            <Navbar />
+
+            <main className="h-full py-8 px-6 flex flex-col">
+              {children}
+            </main>
+          </div>
+        </div>
+
+      </div>
+    </ div>
+  )
+}
+
+export default DashboardLayout
